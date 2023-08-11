94e030b92c9f6c743c76eb9fb7ecf99b3a31ef99
Karanvir Chaudhary <karanvir@hotmail.ca>
2023-08-11 09:04:44 -0400

    styled the page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -26,56 +26,66 @@ const Home = () => {
   };
 
   return (
-    <div>
-      <h1>Parts Counter</h1>
-      <hr />
-      <input
-        type='text'
-        name='newPartName'
-        id='newPartName'
-        onChange={e => setNewPartName(e.target.value)} value={newPartName}
-      />
-      <button
-        onClick={() => handleCreateNewPart()}
-        disabled={!validateNewPartName(newPartName)}
-      >
-        Create Part
-      </button>
-
-      <ul className="partsList">
-        {parts.map(part => (
-          <li
-            key={part.name}
-            onClick={() => setSelectedPart(part.name)}
-            style={{backgroundColor: part.name === selectedPart ? '#42e9f5' : 'white'}}
+    <div className="container">
+      <div className="partsCounterColumn">
+        <h1>Parts Counter</h1>
+        <input
+            type='text'
+            name='newPartName'
+            id='newPartName'
+            placeholder='Enter a name for a new part'
+            onChange={e => setNewPartName(e.target.value)} value={newPartName}
+          />
+          <button
+            onClick={() => handleCreateNewPart()}
+            disabled={!validateNewPartName(newPartName)}
           >
-            {part.name} {part.amount}
-            <button
-              disabled={part.name !== selectedPart}
-              onClick={e => {
-                dispatch(incrementPart(part.name));
-              }}
+            Create Part
+          </button>
+
+        <ul className="partsList">
+          {parts.map(part => (
+            <li
+              key={part.name}
+              onClick={() => setSelectedPart(part.name)}
+              style={{backgroundColor: part.name === selectedPart ? '#42e9f5' : 'white'}}
             >
-              +
-            </button>
-            <button
-              disabled={part.name !== selectedPart}
-              onClick={e => {
-                dispatch(decrementPart(part.name));
-              }}
-            >
-              -
-            </button>
-          </li>
-        ))}
-      </ul>
-      <hr />
-      <h2>Part Info</h2>
-      {selectedPart &&
-        (() => {
-          const part = parts.find(x => x.name === selectedPart);
-          return <PartDescriptor key={part.name} name={part.name} amount={part.amount} />;
-        })()}
+              {part.name}
+
+              <button
+                disabled={part.name !== selectedPart}
+                onClick={e => {
+                  dispatch(incrementPart(part.name));
+                }}
+              >
+                +
+              </button>
+
+              <span style={{marginLeft: 10}}>
+                {part.amount}
+              </span>
+
+              <button
+                disabled={part.name !== selectedPart}
+                onClick={e => {
+                  dispatch(decrementPart(part.name));
+                }}
+              >
+                -
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className='partsInfoColumn'>
+        <h2>Part Info</h2>
+        {selectedPart &&
+          (() => {
+            const part = parts.find(x => x.name === selectedPart);
+            return <PartDescriptor key={part.name} name={part.name} amount={part.amount} />;
+          })()}
+      </div>
     </div>
   );
 };
